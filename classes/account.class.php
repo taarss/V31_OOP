641e2eb31ffd4f07ca458e9036877febac4f0483
christianvillads <chri45n5@edu.campusvejle.dk>
2021-01-21 12:18:09 +0100

    fixed login bug

--- classes/account.class.php
+++ classes/account.class.php
@@ -16,6 +16,20 @@
             $results = $stmt->fetchAll();
             return $results;
         }
+        public function getAccountActivateCode($email, $activasion_code){
+            $sql = "SELECT * FROM accounts WHERE email = ? AND activation_code = ?";
+            $stmt = $this->connect()->prepare($sql);                                                                                                                                                               
+            $stmt->execute([$email, $activasion_code]);
+            $results = $stmt->fetchAll();
+            return $results;
+        }
+        public function getAccountFromUsernameAndEmail($username, $email){
+            $sql = "SELECT * FROM accounts WHERE email = ? AND activation_code = ?";
+            $stmt = $this->connect()->prepare($sql);                                                                                                                                                               
+            $stmt->execute([$username, $email]);
+            $results = $stmt->fetchAll();
+            return $results;
+        }
 
         //controller
         public function updateResetCode($code, $email){
